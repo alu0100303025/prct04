043f219d910c0f527ab750e1a8f72f8b48d3a745
Abraham <abrahamblg@hotmail.es>
2013-10-09 12:39:52 +0100

    Poner las dos funciones

--- multiplicar_matrices.rb
+++ multiplicar_matrices.rb
@@ -14,13 +14,28 @@ def hacer_matriz(filas,columnas, valor)
 	array_devolucion = Array.new(filas){Array.new(columnas, valor)}
 end
 
+def multiplica_matrices(m1,m2)
+        dimensiones=[[m1.length, m1[0].length],[m2.length, m2[0].length]]
+        filas_final = dimensiones[0][0]
+        columnas_final = dimensiones[1][1]
+        resultado = Array.new(filas_final){Array.new(columnas_final, 0)}
+        for i in 0...dimensiones[0][0]
+                for j in 0...dimensiones[1][1]
+                        temp = Array.new(dimensiones[0][0],0)
+                        for k in 0...dimensiones[0][1]
+                                val1 = m1[i][k]
+                                val2 = m2[k][j]
+                                temp[k] += val1 * val2
+                        end
+                        resultado[i][j] = temp.reduce(:+)
+                end
+        end
+        return resultado
+end
 
 
 def suma_matrices(m1,m2)
 	dimensiones=[[m1.length, m1[0].length],[m2.length, m2[0].length]]
-	puts "m1[#{m1.length},#{m1[0].length}]"
-	puts "m2[#{m2.length},#{m2[0].length}]"
-
 	filas_final = dimensiones[0][0]
 	columnas_final = dimensiones[1][1]
 	resultado = Array.new(filas_final){Array.new(columnas_final, 0)}
@@ -47,5 +62,9 @@ muestra_matriz(matriz4)
 muestra_matriz(matriz5)
 
 matriz_suma = suma_matrices(matriz4,matriz5)
-muestra_matriz(matriz_suma)
+matriz_multiplicacion = multiplica_matrices(matriz4,matriz5)
 
+puts("Suma:")
+muestra_matriz(matriz_suma)
+puts("Multiplicacion")
+muestra_matriz(matriz_multiplicacion)
